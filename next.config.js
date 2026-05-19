1ff2b4c9e52f2451e6dd6e77cf9dd044445f8638
pshkai <pshkai.business@gmail.com>
2026-05-19 16:13:44 +0700

    Fix Next.js static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+
+  basePath: "/portfolio",
+
+  assetPrefix: "/portfolio/",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
